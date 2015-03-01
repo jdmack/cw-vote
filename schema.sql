5ab8c554f3bcccc16dbc208edaab426fd6c049bb
James Mack <github@wulph.com>
2015-02-28 18:15:49 -0800

    Add schema

---
+++ schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE poll (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(100),
+    type INT REFERENCES poll_type(id),
+    start_date DATETIME,
+    end_date DATETIME
+);
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(20)
+);
+
+CREATE TABLE vote (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    poll INT REFERENCES poll(id),
+    user INT REFERENCES user(id),
+    date DATETIME,
+    option INT REFERENCES option(id),
+    value VARCHAR(10)
+);
+
+CREATE TABLE option (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+);
+
+CREATE TABLE poll_option (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    poll INT REFERENCES poll(id),
+    option INT REFERENCES option(id),
+);
+
+CREATE TABLE poll_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+);
+
+
+
+    
